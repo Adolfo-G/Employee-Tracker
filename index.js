7dc4fbd1a106f32ed314eccbc6d7105d6aa1fb23
Adolfo Gonzalez <agonzal177@gmail.com>
2022-02-25 19:08:32 -0500

    added view employees add department and add role

--- index.js
+++ index.js
@@ -75,4 +75,90 @@ function viewRole() {
     showRole()
 }
 
+function viewEmployees() {
+    const showEmployees = () => {
+        sql.query("SELECT employees.id,first_name,last_name,title,department,salary,employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id;", (err, results) => { console.table(results) });
+        setTimeout(() => {
+            start();
+        }, 100);
+    }
+    showEmployees()
+}
+
+function addDept() {
+    const newDept = () => {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "deptName",
+                message: "Please enter a new Department name to add:"
+            }
+        ])
+    }
+    newDept()
+        .then((data) => {
+            sql.query(`INSERT INTO departments(department) VALUES("${data.deptName}")`, (err, results) => { console.log("Department Added") })
+        })
+        .then((data) => {
+            sql.query(`SELECT *FROM departments`, (err, results) => { console.table(results) })
+        })
+        .then((data) => {
+            setTimeout(() => {
+                start();
+            }, 100);
+        })
+}
+function addRole() {
+    const newRole = () => {
+        let dtd = []
+        sql.query("select * from departments", (err, results) => { addDTd(results) })
+        function addDTd(value) {
+            for (let i = 0; i < value.length; i++) {
+                dtd.push({
+                    name: value[i]["department"],
+                    value: value[i]["id"]
+                })
+            }
+        }
+        console.log(dtd)
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "roleName",
+                message: "Please enter a new role to add:"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Please enter this role's salary:"
+            },
+            {
+                type: "list",
+                name: "deptChoice",
+                message: "Please select of the following:",
+                choices: dtd
+            }
+        ])
+    }
+    newRole()
+        .then((data) => {
+            console.log(data)
+            sql.query(`INSERT INTO roles(title, salary, department_id) VALUES("${(data.roleName)}","${data.salary}","${data.deptChoice}")`, (err, results) => {
+                if (err) {
+                    throw err
+                }
+                console.log(results)
+            })
+        })
+        .then((data) => {
+            sql.query(`SELECT *FROM roles`, (err, results) => { console.table(results) })
+        })
+        .then((data) => {
+            setTimeout(() => {
+                start();
+            }, 100);
+        })
+}
+
+
 start()
